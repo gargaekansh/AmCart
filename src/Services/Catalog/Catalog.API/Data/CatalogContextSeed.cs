16edf03c0aff78cf51675d43245bcf342feec8eb
Akansh Garg <akanshga@nedbank.co.za>
2025-01-31 23:45:05 +0530

    commit for adding additional products

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -9,7 +9,7 @@ namespace Catalog.API.Data
 {
     public class CatalogContextSeed
     {
-        public static  void SeedData(IMongoCollection<Product> productCollection)
+        public static void SeedData(IMongoCollection<Product> productCollection)
         {
             bool existProduct = productCollection.Find(p => true).Any();
             if (!existProduct)
@@ -19,7 +19,7 @@ namespace Catalog.API.Data
                 {
                     productCollection.InsertMany(products);
                 }
-                catch (Exception ex )
+                catch (Exception ex)
                 {
 
                     throw ex;
@@ -247,6 +247,80 @@ namespace Catalog.API.Data
                 Count = 150
             }
         },
+
+
+    new Product
+    {
+        Id = 16,
+        Name = "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+        Price = 29.95m,
+        Description = "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+        Category = "women's clothing",
+        Image = "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+        Rating = new Rating
+        {
+            Rate = 2.9m,
+            Count = 340
+        }
+    },
+    new Product
+    {
+        Id = 17,
+        Name = "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+        Price = 39.99m,
+        Description = "Lightweight perfect for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it. Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+        Category = "women's clothing",
+        Image = "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+        Rating = new Rating
+        {
+            Rate = 3.8m,
+            Count = 679
+        }
+    },
+    new Product
+    {
+        Id = 18,
+        Name = "MBJ Women's Solid Short Sleeve Boat Neck V",
+        Price = 9.85m,
+        Description = "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+        Category = "women's clothing",
+        Image = "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+        Rating = new Rating
+        {
+            Rate = 4.7m,
+            Count = 130
+        }
+    },
+    new Product
+    {
+        Id = 19,
+        Name = "Opna Women's Short Sleeve Moisture",
+        Price = 7.95m,
+        Description = "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+        Category = "women's clothing",
+        Image = "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+        Rating = new Rating
+        {
+            Rate = 4.5m,
+            Count = 146
+        }
+    },
+    new Product
+    {
+        Id = 20,
+        Name = "DANVOUY Womens T Shirt Casual Cotton Short",
+        Price = 12.99m,
+        Description = "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring, Summer, Autumn, Winter.",
+        Category = "women's clothing",
+        Image = "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+        Rating = new Rating
+        {
+            Rate = 3.6m,
+            Count = 145
+        }
+    },
+
+
     };
         }
 
